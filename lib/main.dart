d27eddc171bc31f830e00cdc357c10f1dcec5346
Janak R Khadka <rmajrk96@icloud.com>
2020-12-28 16:22:09 -0800

    Installed plugins

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 void main() {
   runApp(MyApp());
